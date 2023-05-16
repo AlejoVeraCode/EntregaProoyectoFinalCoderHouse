162d31cfd0a4f0bb063e2b43eff762dd8cab14e2
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-05-16 04:24:12 -0300

    Update CheckOutForm.js
    Se soluciona bug que no devolvia el id de la orden de compra, tambien para una mejor navegabilidad el formulario aparece como un pop-up despues del resumen de compra.

--- src/Components/CheckOutForm/CheckOutForm.js
+++ src/Components/CheckOutForm/CheckOutForm.js
@@ -1,43 +1,68 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import styles from './CheckOutForm.module.css'
 
+const CheckOutForm = ({ onConfirm, onClose }) => {
+    const inputNombre = useRef(null);
+    const inputTelefono = useRef (null);
+    const inputDireccion = useRef (null);
+    const inputEmail = useRef (null);
 
-const CheckOutForm = ({ onConfirm }) => {
-    const [nombre, setNombre] = useState('')
-    const [telefono, setTelefono] = useState('')
-    const [direccion, setDireccion] = useState ('')
-    const [email, setEmail] = useState('')
-
-    const handleConfirm = (e) => {
-        e.preventeDefault()
+    const handleConfirm = (event) => {
+        event.preventDefault()
 
         const userData = {
-            nombre, telefono, direccion, email
+            nombre: inputNombre.current.value,
+            telefono: inputTelefono.current.value,
+            direccion: inputDireccion.current.value,
+            email: inputEmail.current.value,
         }
-
-        onConfirm(userData)
+        onConfirm(userData, "orders");
+        onClose();
     }
 
     return (
         <div className={styles.Container}>
             <form onSubmit={handleConfirm} className={styles.FormAddUser}>
                 <label> Nombre: 
-                <input type="text" value={nombre} onChange={({ target }) => setNombre(target.value)} />
+                <input type="text" ref= {inputNombre} placeholder="Ingrese nombre"/>
                 </label>
                 <label> Telefono:
-                <input type="text" value={telefono} onChange={({ target }) => setTelefono(target.value)} />
+                <input type="text" ref= {inputTelefono} placeholder="Ingrese telefono"/>
                 </label>
                 <label> Direccion: 
-                <input type="text" value={direccion} onChange={({ target }) => setDireccion(target.value)} />
+                <input type="text" ref= {inputDireccion} placeholder="Ingrese direccion" />
                 </label>
                 <label> Email: 
-                <input type="text" value={email} onChange={({ target }) => setEmail(target.value)} />
+                <input type="text" ref= {inputEmail} placeholder="Ingrese e-mail"/>
                 </label>
-                <button type="submit">Finalizar</button>
+                <button className={styles.Button} type="submit">Finalizar</button>
             </form>
-        
+            <button className={styles.ButtonClose} onClick={onClose}>Cerrar</button>
         </div>
     )
 }
 
-export default CheckOutForm
+const CheckOutPopUp = ({ onConfirm, onClose }) => {
+    const [showPopUp, setShowPopUp] = useState(false);
+
+    const handleShowPopUp = () => {
+        setShowPopUp(true);
+    }
+
+    const handleClosePopUp = () => {
+        setShowPopUp(false);
+    }
+
+    return (
+        <div>
+            <button className={styles.Button} onClick={handleShowPopUp}>Finalizar compra</button>
+            {showPopUp &&
+                <div style={{position: 'fixed', top: 0, bottom: 0, left: 0, right: 0}}>
+                    <CheckOutForm onConfirm={onConfirm} onClose={handleClosePopUp} />
+                </div>
+            }
+        </div>
+    )
+}
+
+export default CheckOutPopUp;
