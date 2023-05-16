e7253c8055c6b008a71b048e0df7cb53130f57e7
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-05-16 04:16:51 -0300

    Update CartWidget.js
    Se soluciona bug que no mostraba el icono del CartWidget cuando habia productos.

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -5,11 +5,12 @@ import { CartContext } from "../../Context/CartContext";
 
 const CartWidget = () => {
 
-  const {totalQuantity} = useContext (CartContext)
+  const {totalQuantity, total} = useContext (CartContext)
+  console.log(totalQuantity)
 
   return (
-      <Link to='/cart'>
-      <img className={styles.img} src="https://i.postimg.cc/vBXNx1pz/carrito.png" alt="Carrito" width={30} />{totalQuantity}
+    <Link to="/cart" style={{ display: total> 0 ? "block" : "none" }}>
+      <img className={styles.img} src="https://i.postimg.cc/vBXNx1pz/carrito.png" alt="Carrito" width={30} /><h3 className={styles.Quantity} >{totalQuantity()}</h3>
       </Link>
   );
 };
